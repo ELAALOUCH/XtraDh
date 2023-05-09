a411743933b48292106f06e759b2c205708fc9a7
GLIOUAL YASSINE <glioualyassine@gmail.com>
2023-05-09 17:28:50 +0100

    create: creation des route de API REST

--- projet/routes/api.php
+++ projet/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EtablissementController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\EnseignantController;
+use App\Http\Controllers\AdministrateurController;
+use App\Http\Controllers\InterventionController;
+use App\Http\Controllers\PaiementController;
+use App\Http\Controllers\userController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/* Etablissement Routes */
+Route::apiResource('Etablissement',EtablissementController::class); 
+
+/*Grade Routes */ 
+Route::apiResource('Grade',GradeController::class);
+
+/* Enseignant routes */
+
+Route::apiResource('Enseignant',EnseignantController::class);
+
+/* Administrateur routes */
+
+Route::apiResource('Administrateur',AdministrateurController::class);
+
+/* Intervention Routes */
+Route::apiResource('Intervention',InterventionController::class);
+
+/* Paiement Routes */
+Route::apiResource('Paiement',PaiementController::class);
+ 
+/* Uusers Routes */
+Route::apiResource('User',userController::class);
+
+
