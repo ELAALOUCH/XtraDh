7e0b16e36e93021d8e2688a769703a73228a3529
Soufian Bouktaib <109437519+sofexbk@users.noreply.github.com>
2023-05-13 01:11:17 +0100

    add: dashboards added

---
+++ Frontend/vueproject/src/Interfaces/President_Univ/Dash-pu.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="w-screen h-screen flex">
+      <!-- Side bar -->
+      <div class="w-[400px] h-full bg-blue-200 text-white" v-show="showSide">
+        <div class="h-[50px] bg-blue-950 flex justify-start  items-center ">
+          <div class="px-[20px]">
+            <h3 class="font-bold text-xl">Admin Dashboard</h3>
+          </div>
+        </div>
+        <div class="h-[calc(100vh-50px)] bg-blue-800 py-[20px]">
+          <div class="flex flex-col justify-between h-full px-[20px] space-y-[10px]">
+            <div class=" flex flex-col justify-between space-y-[10px]">
+              
+                <router-link to="/homep" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+                <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
+                </svg>
+                Home
+              </router-link>
+              
+              <router-link to="/profilep" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-300  hover:text-blue-800 transition duration-400 ease-in-out">
+                <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
+                </svg>
+                Profile
+              </router-link>           
+            </div>
+  
+  
+            
+            <div class="h-[50px]">
+              <div>             
+                <router-link to="/logout" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300 hover:text-gray-800  transition duration-400 ease-in-out">
+                  <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
+                  </svg>
+                  Déconnexion
+                </router-link>              
+            </div>
+            </div>
+  
+  
+  
+          </div>
+        </div>
+      </div>
+      <div class="w-full h-full bg-blue-400">
+        <div class="h-[50px] bg-blue-100 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b ">
+          <!-- Hambuger menu -->
+          <div class="cursor-pointer w-[30px]" @click="toggleSideBar">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class=" w-[25px] h-[25px]">
+              <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+            </svg>
+          </div>
+          <!-- Search bar -->
+  
+          <div class="w-[calc(100%-30px)] flex">
+            <div class="w-[calc(100%-200px)] flex justify-center ">
+              <!-- Search bar -->    
+            </div>
+            <!-- User login -->
+            <div class="w-[200px] ">
+              <div class="flex items-center justify-start space-x-4" @click="toggleDrop">
+                <img class="w-10 h-10 rounded-full border-2 border-blue-50" src="@/assets/images/download.jpg">
+                <div class="font-semibold dark:text-white text-left">
+                  <div>Madona ,Dev OP</div>
+                  <div class="text-xs text-blue-500 dark:text-blue-400">Admin</div>
+                </div>
+              </div>
+              <!-- Drop down -->
+              <div v-show="showDropDown" class="absolute right-[10px] z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                <div class="py-1 text-left" role="none">
+                  <!-- Active: "bg-blue-100 text-blue-900", Not Active: "text-blue-700" -->
+                  <a href="#" class="text-blue-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
+                  <a href="#" class="text-blue-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
+                  <a href="#" class="text-blue-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+                  <form method="POST" action="#" role="none">
+                    <button type="submit" class="text-blue-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="h-[calc(100vh-50px)] bg-blue-50 p-[20px]">
+          <div class="border border-blue-300 rounded-md p-[20px] h-full">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+      <!-- Main  -->
+    </div>
+  </template>
+   <script>
+   export default {
+     data() {
+       return {
+         showDropDown: false,
+         showSide: true
+       }
+     },
+     methods: {
+       // hide show side bar
+       toggleSideBar() {
+         this.showSide = !this.showSide
+   
+       },
+       // toggle user 
+       toggleDrop() {
+         this.showDropDown = !this.showDropDown
+   
+       }
+     }
+   
+   }
+  </script>
+  
