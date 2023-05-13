5345c08e1566760e8b4d72caca079df106ec9844
Soufian Bouktaib <109437519+sofexbk@users.noreply.github.com>
2023-05-13 09:22:45 +0100

    update : interfaces updated

--- Frontend/vueproject/src/router/index.js
+++ Frontend/vueproject/src/router/index.js
@@ -5,6 +5,11 @@ import Forgetpassword from '@/Pages/auth/Forgetpassword.vue'
 import Resetpassword from '@/Pages/auth/Resetpassword.vue'
 import Wait from '@/Pages/auth/Wait.vue'
 import Dash_au from '@/Interfaces/Admin_Univ/Dash_au.vue'
+import Dash_ae from '@/Interfaces/Admin_Etab/Dash_ae.vue'
+import Dash_de from '@/Interfaces/Directeur_Etab/Dash_de.vue'
+import Dash_pu from '@/Interfaces/President_Univ/Dash_pu.vue'
+import Dash_users from '@/Interfaces/Users/Dash_users.vue'
+
 const routes = [
   {
     path: '/',
@@ -30,6 +35,26 @@ const routes = [
     path:'/Dash_au',
     name:'Dash_au',
     component:Dash_au
+  },
+  {
+    path:'/Dash_ae',
+    name:'Dash_ae',
+    component:Dash_ae
+  },
+  {
+    path:'/Dash_de',
+    name:'Dash_de',
+    component:Dash_de
+  },
+  {
+    path:'/Dash_pu',
+    name:'Dash_pu',
+    component:Dash_pu
+  },
+  {
+    path:'/Dash_users',
+    name:'Dash_users',
+    component:Dash_users
   },
    {
     path:'/:catchAll(.*)',
