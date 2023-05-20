2006c4b804531e13b7a2a3ee4fe5f37609b8dcab
GLIOUAL YASSINE <glioualyassine@gmail.com>
2023-05-20 20:01:46 +0100

    add email to users controller

--- Backend/app/Http/Controllers/userController.php
+++ Backend/app/Http/Controllers/userController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class userController extends Controller
 {
@@ -34,11 +37,17 @@ class userController extends Controller
             'password' =>'string|confirmed|required',
             'type'=>'required'
         ]);
+        $fields['password']=Str::random(15);
         $user = User::create([
             'type' =>$fields['type'],
             'email' => $fields['email'],
             'password'=>bcrypt($fields['password'])
         ]);
+        $email = $fields['email'];
+        Mail::send('Mails.password',['password'=>$fields['password']],function(Message $message)use($email){
+            $message->to($email);
+            $message->subject('Voici le mot de pass de votre compte hsup');
+        });
         $token = $user->createToken('MyAppToken')->plainTextToken;
         $response= [
             'user'=>$user,
