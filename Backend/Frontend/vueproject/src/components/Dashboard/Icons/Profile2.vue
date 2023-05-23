7e3d94d564622f619c15fed8c86bc2622f01c3a7
MOHAMMED YASSINE EL AALOUCH <elaalouch.dev@gmail.com>
2023-05-23 14:21:57 +0100

    clean files

--- Backend/Frontend/vueproject/src/components/Dashboard/Icons/Profile2.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <svg class="h-8 w-8 text-white-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  
-    <circle cx="12" cy="7" r="4" /> 
-     <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-     </svg>
-</template>
