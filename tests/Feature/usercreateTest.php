7c3d85be7557903e4508891ea7434e24f5f182a6
MOHAMMED YASSINE EL AALOUCH <125577100+ELAALOUCH@users.noreply.github.com>
2023-05-23 09:54:19 +0100

    Update usercreateTest.php
    Signed-off-by: MOHAMMED YASSINE EL AALOUCH <125577100+ELAALOUCH@users.noreply.github.com>

--- tests/Feature/usercreateTest.php
+++ tests/Feature/usercreateTest.php
@@ -13,8 +13,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class testons extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $this->artisan('migrate');
+    }
 public function testCreateUsers()
-{
+
+{ 
     $users = User::factory()->count(10)->create();
     echo $users[0]['email'];
     $users = etablissement::factory()->count(10)->create();
@@ -23,8 +29,14 @@ public function testCreateUsers()
     $users = Paiement::factory()->count(10)->create();
     $users = Administrateur::factory()->count(10)->create();
     $users = Enseignant::factory()->count(10)->create();
+    
+    }
+    public function tearDown(): void
+    {
+        $this->artisan('migrate:rollback');
+        parent::tearDown();
+    }
 
 
-}
 }
 ?>
