9599aea93274d186ba51b2da5ccb4697472653a1
echberaal <echberaalmail@gmail.com>
2023-05-24 19:21:41 +0100

    controlleur user

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -20,7 +20,7 @@ class userController extends Controller
 
     public function __construct()
     {
-    $this->middleware('auth:sanctum')->except(['store']);
+    //$this->middleware('auth:sanctum')->except(['store']);
     }
 
     public function index()
@@ -53,11 +53,11 @@ class userController extends Controller
         ]);
        // 'email' => Crypt::encrypt($fields['email']),
      //   $email = Crypt::decrypt($fields['email']);
-     $email = $fields['email'];
-        // Mail::send('Mails.password',['password'=>$fields['password']],function(Message $message)use($email){
-        //     $message->to($email);
-        //     $message->subject('Voici le mot de pass de votre compte hsup');
-        // });
+        $email = $fields['email'];
+         Mail::send('Mails.password',['password'=>$fields['password']],function(Message $message)use($email){
+            $message->to($email);
+             $message->subject('Voici le mot de pass de votre compte hsup');
+        });
          $token = $user->createToken('MyAppToken')->plainTextToken;
         $response= [
             'user'=>$user,
