9c14f2044d5a470feab0e7a02a37f2df0f70efb7
GLIOUAL YASSINE <glioualyassine@gmail.com>
2023-06-10 00:59:28 +0100

    conn admin_etb with backend

--- Frontend/vueproject/src/Interfaces/Admin_Etab/GestionaI.vue
+++ Frontend/vueproject/src/Interfaces/Admin_Etab/GestionaI.vue
@@ -1,114 +1,104 @@
 <template>
-  <div>
-    <div class="overflow-x-auto relative  sm:rounded-lg  overflow-y-auto h-[560px] w-[1200px] ">
-      <table class="w-full text-sm text-left text-gray-500 ">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
-          <tr>
-            <th scope="col" class="py-3 px-6">
-              PPR
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex items-center">
-                Nom
-              </div>
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex items-center">
-                Prenom             
-              </div>
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex items-center">
-                Date_naissance
-              </div>
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex items-center">
-                Etablissement
-              </div>
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex items-center">
-                Grade
-              </div>
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex items-center">
-                Email
-              </div>
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex items-center">
-                Type
-              </div>
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <div class="flex justify-end" >
-                <Create/>
-              </div> 
-           </th>             
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="bg-white border-b  " v-for="data in profs " :key="data.id">
-            <th class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
-             {{ data.PPR }}
-            </th>
-            <td class="py-4 px-6" >
-              {{ data.Nom }}
-            </td>
-            <td class="py-4 px-6">
-              {{ data.prenom }}
-            </td>
-            <td class="py-4 px-6">
-              {{ data.Date_Naissance }}
-            </td>
-            <td class="py-4 px-6" >
-              {{ data.etab_permanant.Nom }}
-            </td>
-            <td class="py-4 px-6" >
-              {{ data.grade.designation }}
-            </td>
-            <td class="py-4 px-6" >
-              {{ data.user.email }}
-            </td>
-            <td class="py-4 px-6">
-              {{ data.user.type }}
-            </td>
-            <td class="py-4 px-6 text-right">
-              <div class="inline-flex">
-               <Edit/>
-               <button class="bg-red-500 hoover:bg-blue-400 text-white font-bold py-2 px-4 rounded-i" @click="">
-                 Delete
-               </button>                  
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div >
+      <h5><p class="text-4xl font-medium text-gray-600 mb-8">Ajouter Intervention</p></h5>
+    <div v-if="test">
+      <div class="flex p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+        <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+        <span class="sr-only">Info</span>
+        <div>
+          <span class="font-medium">Info alert!</span> Intervention Crée avec Succes.
+        </div>
+      </div>
+    </div>
+<form @submit.prevent="handlesubmit()">
+  <div class="relative z-0 w-full mb-6 group">
+      <input v-model="PPR" type="text" name="PPR" id="PPR" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <label for="PPR" class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">PPR</label>
+  </div>
+  <div class="relative z-0 w-full mb-6 group">
+      <input v-model="Intitule_Intervention" type="text" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Intitulé intervention</label>
+  </div>
+  <div class="relative z-0 w-full mb-6 group">
+      <input v-model="Nbr_heures" type="number" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nombre Heure</label>
+  </div>
+  <div class="">
+    <div class="relative z-0 w-full mb-6 group md:gap-6">
+        <input v-model="Annee_univ" type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Année Scolaire</label>
+    </div>
+    <div class="relative z-0 w-full mb-6 group">
+      
+      <select  required v-model="Semestre" id="underline_select" class="block py-2.5 px-1.5 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+        <option disabled selected value="">Sélectionnez une semestre</option>
+          <option value="Semestre 1" >Semestre 1</option>
+          <option value="Semestre 2">Semestre 2</option>
+          
+      </select>
+
+      
+  
     </div>
   </div>
+  <div class="grid md:grid-cols-2 md:gap-6">
+    <div class="relative z-0 w-full mb-6 group">
+        <input v-model="Date_debut" type="date" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date Debut</label>
+    </div>
+    <div class="relative z-0 w-full mb-6 group">
+        <input v-model="Date_fin" type="date" name="floating_company" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date Fin</label>
+    </div>
+  </div>
+  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
+  </div>
+
+
 </template>
 
 <script>
 import axios from 'axios';
-import Create from '@/components/Dashboard/Intervention/Create.vue'
 export default {
-  components:{Create},
-  data() {
-    return {
-      formData: {
-        PPR: '',
-
-      },
-      success: ''
-    };
-  },
-  methods: {
-    handleSubmit() {
-      
+    data(){
+      return {
+        PPR:'',
+        Intitule_Intervention:'',
+        Annee_univ:'',
+        Date_debut:'',
+        Date_fin:'',
+        Nbr_heures:'',
+        Semestre : '',
+        test : 0 
+      }
     },
-  },
+    methods:{
+      async handlesubmit(){
+          const response = await axios.post('/ajoutinterventionetab',{
+            PPR:this.PPR,
+            Intitule_Intervention:this.Intitule_Intervention,
+            Annee_univ:this.Annee_univ,
+            Date_debut:this.Date_debut,
+            Date_fin:this.Date_fin,
+            Nbr_heures:this.Nbr_heures,
+            Semestre : this.Semestre
+                    }).then(()=>{
+                      this.PPR='',
+                      this.Intitule_Intervention='',
+                      this.Annee_univ='',
+                      this.Date_debut='',
+                      this.Date_fin='',
+                      this.Nbr_heures='',
+                      this.Semestre= ''
+                      this.test = 1 ; 
+                    })
+          console.log(response)
+      }
+    }
+}
+
+
 
 
  /*getNonce() {
@@ -137,5 +127,5 @@ export default {
     created(){
       this.getNonce();
     }*/
-    }
+    
 </script>
--- Frontend/vueproject/src/Interfaces/Admin_Etab/Gestionde.vue
+++ Frontend/vueproject/src/Interfaces/Admin_Etab/Gestionde.vue
@@ -40,10 +40,10 @@
           <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 " v-for="data in presi " :key="data.id">
               <th class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-               {{ data.user.email }}
+               {{ data.email }}
               </th>
               <td class="py-4 px-6">
-                {{data.user.email}}
+                {{data.type}}
               </td>
               <td class="py-4 px-6">
                 {{ data.PPR }}
@@ -55,7 +55,7 @@
                {{ data.prenom }}
               </th>
               <th class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-               {{ data.etablissement.Nom }}
+               {{ data.etab_Nom }}
               </th>
 
               <td class="py-4 px-6 text-right">
@@ -89,7 +89,7 @@
   },
   async mounted(){
     try {
-      await axios.get('/administrateur ').then(res=>{   
+      await axios.get('/directeuretab ').then(res=>{   
       console.log(res)
       this.presi=res.data
       })
