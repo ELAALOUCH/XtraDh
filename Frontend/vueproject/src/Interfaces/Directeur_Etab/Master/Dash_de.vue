5c42ae85064795cb49ec5aa880fc772a1a861fd0
MOHAMMED YASSINE EL AALOUCH <elaalouch.dev@gmail.com>
2023-06-11 23:45:54 +0100

    last edit

--- Frontend/vueproject/src/Interfaces/Directeur_Etab/Master/Dash_de.vue
+++ Frontend/vueproject/src/Interfaces/Directeur_Etab/Master/Dash_de.vue
@@ -24,7 +24,7 @@
 
             <router-link to="/Consodp" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
                <paiement class="px-1" />
-              Consultation de paiements
+              Consultation des paiements
             </router-link>
 
             <router-link to="/Profilede" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
