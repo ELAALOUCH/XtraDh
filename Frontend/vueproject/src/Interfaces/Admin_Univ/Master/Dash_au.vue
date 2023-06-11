a23140ff2cc9ceb57576eaba781d65ef76abd2c7
MOHAMMED YASSINE EL AALOUCH <elaalouch.dev@gmail.com>
2023-06-11 21:23:05 +0100

    ges de presi[D[D[D[D[D[D[D[D[D[D[Ction [C[Dde [F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cpresident univ

--- Frontend/vueproject/src/Interfaces/Admin_Univ/Master/Dash_au.vue
+++ Frontend/vueproject/src/Interfaces/Admin_Univ/Master/Dash_au.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-screen h-screen flex">
+  <div class="w-screen overflow-y-hidden h-screen flex">
 
     <div class="w-[400px] h-full bg-blue-200 text-white" v-show="showSide">
       <div class="h-[50px] bg-blue-950 flex justify-start  items-center ">
         <div class="px-[20px]">
-          <h3 class="font-bold text-xl">Admin d'université</h3>
+          <h3 class="font-serif text-xl">Admin d'université</h3>
         </div>
       </div>
       <div class="h-[calc(100vh-50px)] bg-blue-800 py-[20px]">
@@ -13,43 +13,43 @@
           <div class=" flex flex-col justify-between space-y-[10px]">
 
 
-            <router-link to="/Gestionpu" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
-                <president/> Gestion de président universitaire
+            <router-link to="/Gestionpu" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+                <president class="px-1"  /> Gestion de président universitaire
             </router-link>
 
-            <router-link to="/Gestionae" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
-                <interventions/> Gestion d'admins d'établissements
+            <router-link to="/Gestionae" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+                <interventions class="px-1"  /> Gestion d'admins d'établissements
             </router-link>
 
-            <router-link to="/Gestiondeau" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
-            <interventions />
-            Gestion des directeurs d'Etablissement
+            <router-link to="/Gestiondeau" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+            <interventions class="px-1"  />
+            Gestion des directeurs 
             </router-link>
 
-            <router-link to="/Gestionpau" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
-            <interventions />
+            <router-link to="/Gestionpau" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+            <interventions class="px-1"   />
             Gestion des profs
             </router-link>
 
-            <router-link to="/Gestioninterv" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
-            <interventions />
+            <router-link to="/Gestioninterv" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+            <interventions class="px-1"  />
             Gestion des interventions
             </router-link>
 
             
-            <router-link to="/Gestiongrade" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
-            <Grade />
+            <router-link to="/Gestiongrade" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+            <Grade class="px-1"  />
             Gestion de grade
             </router-link>
 
-            <router-link to="/Gestionetab" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
-            <Shcool />
+            <router-link to="/Gestionetab" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-200 hover:text-blue-800  transition duration-400 ease-in-out">
+            <Shcool class="px-1"  />
             Gestion des etablissements
             </router-link>
 
 
-            <router-link to="/Profileau" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-blue-200 hover:bg-blue-300  hover:text-blue-800 transition duration-400 ease-in-out">
-             <Profile/> Profil
+            <router-link to="/Profileau" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-serif rounded-md border-blue-200 hover:bg-blue-300  hover:text-blue-800 transition duration-400 ease-in-out">
+             <Profile class="px-1" /> Profil
             </router-link>
 
 
@@ -83,18 +83,25 @@
             <!-- Search bar -->
           </div>
           <!-- User login -->
-          <div class="w-[200px] ">
-            <div class="flex items-center justify-start space-x-4" @click="toggleDrop">
-              <img class="w-10 h-10 rounded-full border-2 border-blue-50" src="@/assets/images/download.jpg">
-              <div class="font-semibold dark:text-white text-left">
-
-
-
-                <div v-if="authenticated">{{ user.email }}</div>
-                <div class="text-xs text-blue-500 dark:text-blue-400">Admin</div>
-              </div>
+          <ul role="list" class="max-w-sm divide-y divide-gray-200">
+            <li class="py-3 sm:py-4">
+            <div class="flex items-center space-x-3">
+            <div class="flex-shrink-0">
+                <img class="w-8 h-8 rounded-full" src="@/assets/images/download.jpg" alt="Neil image">
             </div>
+            <div class="flex-1 min-w-0">
+
+                <p class="text-l text-gray-500 font-serif truncate " v-if="authenticated">
+                  {{ user.email }}
+                </p>
+            </div>
+            <span class="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+                <span class="w-2 h-2 mr-1 bg-green-500 rounded-full"></span>
+                Admin
+            </span>
           </div>
+          </li>
+         </ul>  
         </div>
       </div>
       <div class="h-[calc(100vh-50px)] bg-blue-50 p-[20px]">
