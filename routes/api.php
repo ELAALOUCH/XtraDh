2fb4192cdda565e9039bb8fc2db727230e9c7223
MohamedElHaddad03 <130052591+MohamedElHaddad03@users.noreply.github.com>
2023-06-12 13:19:22 +0100

    gestion des roles

--- routes/api.php
+++ routes/api.php
@@ -11,147 +11,124 @@ use App\Http\Controllers\PaiementController;
 use App\Http\Controllers\userController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ForgetController;
-use App\Models\Enseignant;
-use Spatie\Csp\Nonce\NonceGenerator;
-use Doctrine\Instantiator\InstantiatorInterface;
-/*
-Route::get('/api/get-nonce', function () {
-    $nonce = app(NonceGenerator::class)->generate();
 
-    return response()->json([
-        'nonce' => $nonce,
-    ]);
-});*/
+// Routes publiques
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot', [ForgetController::class, 'forgot']);
+Route::post('/reset', [ForgetController::class, 'reset']);
 
-/* Paiement Routes */
-Route::apiResource('paiement',PaiementController::class);
-Route::get('/paiementprof',[PaiementController::class,'paiementprof'])->middleware("auth:sanctum");
-Route::get('/historiquepdfpaie',[PaiementController::class,'historiquepdfpaie'])->middleware("auth:sanctum");
-Route::get('/consultpaiementetabdirecteur',[PaiementController::class,'consultpaiementetabdirecteur'])->middleware("auth:sanctum");
+// Routes authentifiées
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/adminprofile', [userController::class, 'adminProfile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
 
+// Routes pour le rôle "admin_univ"
+Route::middleware(['auth:sanctum','role:admin_univ'])->group( function () {
+    // Routes protégées pour admin_univ
 
-
-
-/** AUTH ROUTE */
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/forgot',[ForgetController::class,'forgot']);
-Route::post('/reset',[ForgetController::class,'reset']);
-
-
-
-/**  Users Routes */
-Route::apiResource('user',userController::class);
-Route::post('/storeprofetb',[userController::class,'storeProfEtb'])->middleware("auth:sanctum");
-Route::post('/storeadminetb',[userController::class,'storeAdmEtb'])->middleware("auth:sanctum");
-Route::patch('/updateprof/{idprof}',[userController::class,'updateprof'])->middleware("auth:sanctum");
-Route::patch('/updateadm/{idAdm}',[userController::class,'updateAdm'])->middleware("auth:sanctum");
-Route::post('/ajoutinterventionetab',[userController::class,'ajoutinterventionetab'])->middleware("auth:sanctum");
-Route::get('/adminprofile',[userController::class,'adminProfile'])->middleware("auth:sanctum");
-Route::get('/profprofile',[userController::class,'profProfile'])->middleware("auth:sanctum");
-Route::delete('/deleteadm/{id_user}',[userController::class,'destroyadmin']);
-
-
-
-//protected routes
-Route::group(['middleware'=>['auth:sanctum']], function () {
-    Route::delete('/logout',[AuthController::class,'logout']);
-    Route::get('/user-profile',[AuthController::class,'userProfile']);
+    Route::get('/directeuretab', [AdministrateurController::class, 'directeurETB']);
+    Route::get('/listeAdminETBforadminuae', [AdministrateurController::class, 'listeAdminETBforadminuae']);
+    Route::get('/listepresidentuaeforadminuae', [AdministrateurController::class, 'listepresidentuaeforadminuae']);
+    Route::get('/listedirecteuretbforadminuae', [AdministrateurController::class, 'listedirecteuretbforadminuae']);
 
 });
 
 
-/*
-//Route::middleware('auth:sanctum')->get('/api/profile', function (Request $request) {
-    $user = $request->user();
-    // Use $user to retrieve user information or perform actions
-    return response()->json($user);
-//});
-*/
-
-//Route::middleware(['auth:sanctum','role:admin_univ'])->group( function () {
-    //protected for admin univ
-    /* Etablissement Routes */
-    Route::apiResource('etablissement',EtablissementController::class);
 
 
 
+// Routes pour le rôle "directeur_etb"
 
-    /*Grade Routes */
-Route::apiResource('grade',GradeController::class);
+Route::middleware(['auth:sanctum', 'role:directeur_etb'])->group(function () {
 
-
-
-
-
-
-
-
-Route::apiResource('enseignant',EnseignantController::class);
-/* Administrateur routes */
-
-
-
-Route::apiResource('administrateur',AdministrateurController::class);
-Route::get('/directeuretab',[AdministrateurController::class,'directeurETB'])->middleware('auth:sanctum');
-Route::get('/listeAdminETBforadminuae',[AdministrateurController::class,'listeAdminETBforadminuae']);
-Route::get('/listepresidentuaeforadminuae',[AdministrateurController::class,'listepresidentuaeforadminuae']);
-Route::get('/listedirecteuretbforadminuae',[AdministrateurController::class,'listedirecteuretbforadminuae']);
-
-
-/* Intervention Routes */
-Route::apiResource('intervention',InterventionController::class);
-Route::get('/getintervention',[InterventionController::class,'getprofIntervention'])->middleware('auth:sanctum');
-Route::get('/directeuretabintervall',[InterventionController::class,'directeuretabintervall'])->middleware('auth:sanctum');
-Route::get('/directeuretabintervvalid',[InterventionController::class,'directeuretabintervvalid'])->middleware('auth:sanctum');
-Route::post('/storePPR',[InterventionController::class,'storePPR']);
-//});
-
-//Route::middleware(['auth:sanctum','role:admin_etb'])->group( function () {
-    //protected for admin etb
-    /* Enseignant routes */
-
-Route::apiResource('enseignant',EnseignantController::class);
-/* Intervention Routes */
-Route::apiResource('intervention',InterventionController::class);
-Route::get('/interventionuaevalid',[InterventionController::class,'interventionuaevalid']);
-
-
-
-
-// /* Paiement Routes */
-// Route::apiResource('Paiement',PaiementController::class);
-
-/** GENERATE PDF */
-Route::get('postfix', [PaiementController::class, 'postfix']);
-Route::get('/generate-pdf/{prof}', [PaiementController::class, 'generatePDFprof']);
-
-
-//});
-
-
-
-
-
-//          validation
-//Route::middleware(['auth:sanctum','role:directeur_etb'])->group( function () {
-    //protected for directeur
-    Route::get('/valideretb/{id}',[InterventionController::class,'valideretb']);
-Route::get('/invalideretb/{id}',[InterventionController::class,'invalideretb']);
-
-
-
-
-//});
-
-//Route::middleware(['auth:sanctum','role:presidnt_univ'])->group( function () {
-    //protected for président
-    Route::get('/valideruae/{id}',[InterventionController::class,'valideruae']);
-Route::get('/invalideruae/{id}',[InterventionController::class,'invalideruae']);
-//});
-
-//Route::middleware(['auth:sanctum','role:prof'])->group( function () {
-    //protected for prof
-    Route::apiResource('enseignant',EnseignantController::class);
+    Route::get('/valideretb/{id}', [InterventionController::class, 'valideretb']);
+    Route::get('/invalideretb/{id}', [InterventionController::class, 'invalideretb']);
+    Route::get('/consultpaiementetabdirecteur', [PaiementController::class, 'consultpaiementetabdirecteur']);
+    Route::get('/directeuretabintervall', [InterventionController::class, 'directeuretabintervall']);
+    Route::get('/directeuretabintervvalid', [InterventionController::class, 'directeuretabintervvalid']);
     Route::get('/profetab',[EnseignantController::class,'indexetb'])->middleware('auth:sanctum');
-    Route::delete('/deleteprof/{id_user}',[userController::class,'destroyprof']);
-//});
+
+});
+
+// Routes pour le rôle "presidnt_univ"
+Route::middleware(['auth:sanctum', 'role:presidnt_univ'])->group(function () {
+
+
+    Route::get('/valideruae/{id}', [InterventionController::class, 'valideruae']);
+    Route::get('/invalideruae/{id}', [InterventionController::class, 'invalideruae']);
+
+
+});
+
+// Routes pour le rôle "prof"
+Route::middleware(['auth:sanctum', 'role:prof'])->group(function () {
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::group(['middleware' =>'auth:sanctum' ,'role'=>['admin_univ','admin_etb']],function() {
+    Route::apiResource('etablissement', EtablissementController::class);
+    Route::apiResource('grade', GradeController::class);
+    Route::delete('/deleteadm/{id_user}', [userController::class, 'destroyadmin']);
+    Route::delete('/deleteprof/{id_user}', [userController::class, 'destroyprof']);
+    Route::get('/profetab', [EnseignantController::class, 'indexetb']);
+    Route::get('/interventionuaevalid', [InterventionController::class, 'interventionuaevalid']);
+    Route::get('/directeuretab', [AdministrateurController::class, 'directeurETB'])->middleware('auth:sanctum');
+    Route::post('/ajoutinterventionetab', [userController::class, 'ajoutinterventionetab']);
+});
+
+
+Route::group(['middleware' =>'auth:sanctum' ,'role'=>['admin_univ','admin_etb','directeur_etb','president_univ']],function() {
+    Route::apiResource('administrateur', AdministrateurController::class);
+    Route::post('/storePPR', [InterventionController::class, 'storePPR']);
+    Route::patch('/updateadm/{idAdm}', [userController::class, 'updateAdm']);
+    Route::apiResource('intervention', InterventionController::class);
+});
+
+Route::group(['middleware' =>'auth:sanctum' ,'role'=>['admin_univ','prof','president_univ']],function() {
+    Route::apiResource('Paiement', PaiementController::class);
+    Route::get('/paiementprof', [PaiementController::class, 'paiementprof']);
+
+});
+
+
+Route::group(['middleware' =>'auth:sanctum' ,'role'=>['admin_univ','admin_etb','directeur_etb']],function() {
+    Route::post('/storeprofetb', [userController::class, 'storeProfEtb']);
+    Route::post('/storeadminetb', [userController::class, 'storeAdmEtb']);
+});
+
+
+Route::group(['middleware' =>'auth:sanctum' ,'role'=>['admin_univ','prof','admin_etb']],function() {
+    Route::apiResource('enseignant', EnseignantController::class);
+    Route::patch('/updateprof/{idprof}', [userController::class, 'updateprof']);
+    Route::get('/profprofile', [userController::class, 'profProfile']);
+
+});
+
+
+Route::group(['middleware' =>'auth:sanctum' ,'role'=>['prof','president_univ']],function() {
+    Route::get('/historiquepdfpaie', [PaiementController::class, 'historiquepdfpaie']);
+    Route::get('/getintervention', [InterventionController::class, 'getprofIntervention']);
+});
